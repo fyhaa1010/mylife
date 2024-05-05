7a56b1054f88e9474ef8a214c4e9ceba35b42326
fyhaa <124665072+fyhaa1010@users.noreply.github.com>
2024-05-05 23:56:14 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".a").fadeIn(2000,function(){$(".a").fadeOut(3000)})
+    $(".p").fadeIn(20000,function(){$(".p").fadeOut(3000)})
+    $(".c").fadeIn(40000,function(){$(".c").fadeOut(3000)})
+    $(".d").fadeIn(60000,function(){$(".d").fadeOut(3000)})
+    $(".e").fadeIn(80000,function(){$(".e").fadeOut(3000)})
+    $(".f").fadeIn(100000,function(){$(".f").fadeOut(3000)})
+    $("img").fadeIn(110000)
+    
+    
+    
+})
